17ab5e3627814ba3faba9f30559ff8b8a1373250
Jonathan Lin <jonlin223@gmail.com>
2024-01-21 14:33:45 +1100

    Add locations to pokedex

--- src/lib.rs
+++ src/lib.rs
@@ -6,11 +6,16 @@ use tokio::io::AsyncWriteExt;
 pub struct Pokemon {
     name: String,
     types: Vec<String>,
-    sprite: String
+    sprite: String,
+}
+
+#[derive(Debug, Serialize)]
+pub struct Pokedex<'a> {
+    pub locations: Vec<&'a str>,
+    pub pokemon: Vec<Pokemon>,
 }
 
 pub async fn scrape(url: &str) -> Option<Vec<Pokemon>> {
-
     let res = reqwest::get(url).await.ok()?.text().await.ok()?;
     let doc = Html::parse_document(&res);
     let selector = Selector::parse("div.infocard").ok()?;
@@ -25,12 +30,22 @@ pub async fn scrape(url: &str) -> Option<Vec<Pokemon>> {
     for entry in doc.select(&selector).map(|x| x.html()) {
         let entry_doc = Html::parse_document(&entry);
 
-        let pokemon = entry_doc.select(&name_selector).map(|x| x.inner_html()).next()?;
-        let types = entry_doc.select(&type_selector).map(|x| x.inner_html()).collect::<Vec<_>>();
-        let img = entry_doc.select(&image_selector).map(|x| x.value().attr("src")).next()??;
+        let pokemon = entry_doc
+            .select(&name_selector)
+            .map(|x| x.inner_html())
+            .next()?;
+        let types = entry_doc
+            .select(&type_selector)
+            .map(|x| x.inner_html())
+            .collect::<Vec<_>>();
+        let img = entry_doc
+            .select(&image_selector)
+            .map(|x| x.value().attr("src"))
+            .next()??;
 
         let img_info = img.split("https://img.pokemondb.net/sprites/").nth(1)?;
-        let img_name = String::from(img_info.split("/").nth(0)?) + "_" + img_info.split("/").last()?;
+        let img_name =
+            String::from(img_info.split("/").nth(0)?) + "_" + img_info.split("/").last()?;
         let path = String::from("./pokedex/sprites/") + &img_name;
 
         let img_bytes = client.get(img).send().await.ok()?.bytes().await.ok()?;
@@ -38,7 +53,11 @@ pub async fn scrape(url: &str) -> Option<Vec<Pokemon>> {
         file.write_all(&img_bytes).await.ok()?;
 
         // println!("{} - {:?} - {}", pokemon, types, img_name);
-        pokedex.push(Pokemon { name: pokemon, types: types, sprite: img_name });
+        pokedex.push(Pokemon {
+            name: pokemon,
+            types: types,
+            sprite: img_name,
+        });
     }
 
     Some(pokedex)
--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,48 @@
-use pokedex_scraper::scrape;
+use pokedex_scraper::{scrape, Pokedex};
 
 #[tokio::main]
 async fn main() {
-
     // TODO Just note that you have to edit the name of the file yourself
     let url = "https://pokemondb.net/pokedex/game/black-white";
 
-    // Pick a game
-    // Scrape page associated with that game - add game to dex json
-        // For every entry
-        // get name, type, image name
-        // add under game in dex json
-        // download the png if not already stored in the sprites folder
-    // Storage Format
-        // Pokedex (folder)
-            // dex.json
-                // game -> pokemon -> (type + sprite link)
-            // sprites (folder)
-                // (game)_(name).png
+    // TODO This is a note that for now, locations have to be inputted manually, this is just because I haven't found a good way to do data scraping for this process
+    /* let locations = vec!["Starter", "Twinleaf Town", "Route 201", "Lake Verity",
+    "Route 202", "Route 203", "Oreburgh Gate", "Oreburgh City", "Oreburgh Mine",
+    "Route 204", "Ravaged Path", "Floaroma Meadow",
+    "Valley Windworks", "Route 205", "Eterna Forest", "Eterna City",
+    "Old Chateau", "Route 206", "Wayward Cave", "Route 207",
+    "Mt. Coronet", "Route 208", "Hearthome City", "Route 209",
+    "Solaceon Ruins", "Lost Tower", "Route 210",
+    "Route 215", "Veilstone City",
+    "Route 214", "Maniac Tunnel", "Valor Lakefront", "Route 213", "Pastoria City", "Route 212",
+    "Celestic Town", "Fuego Ironworks", "Route 219", "Route 220",
+    "Route 221", "Route 218", "Canalave City", "Iron Island",
+    "Route 211", "Route 216", "Route 217",
+    "Acuity Lakefront", "Lake Acuity", "Distortion World",
+    "Sendoff Spring", "Lake Valor", "Route 222", "Sunyshore City",
+    "Route 223", "Pokémon League", "Victory Road"
+    ]; */
+
+    let locations = vec!["Starter", "Route 1", "Route 2", "Striaton City", "The Dreamyard",
+    "Route 3", "Wellspring Cave", "Nacrene City", "Pinwheel Forest",
+    "Route 4", "Desert Resort", "Relic Castle",
+    "Route 5", "Driftveil City", "Cold Storage",
+    "Route 6", "Chargestone Cave", "Route 7", "Celestial Tower",
+    "Route 17", "Route 18", "P2 Laboratory", "Mistralton Cave",
+    "Twist Mountain", "Icirrus City", "Dragonspiral Tower",
+    "Route 8", "Moor of Icirrus", "Route 9",
+    "Route 10", "Victory Road"];
 
     match scrape(url).await {
-        Some(pokedex) => {
-            println!("{:?}", pokedex);
+        Some(pokemon) => {
+            println!("{:?}", pokemon);
+
+            let pokedex = Pokedex { locations, pokemon };
             let json = serde_json::to_string(&pokedex).expect("Bad");
             let file = std::fs::File::create("./pokedex/black-white.json").expect("File not created?");
-            serde_json::to_writer_pretty(file, &json).expect("Something went wrong with serialisation");
-        },
-        None => println!("Something went wrong I guess")
+            serde_json::to_writer_pretty(file, &json)
+                .expect("Something went wrong with serialisation");
+        }
+        None => println!("Something went wrong I guess"),
     };
-
 }
